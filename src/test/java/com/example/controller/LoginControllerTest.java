2a999e00fa018fa31dc656bf23aafd5f91e719fd
Edward Ramos <kalbzero.ramos@gmail.com>
2017-06-01 09:11:54 -0300

    Fetch(Files): New project for Sprint4

---
+++ src/test/java/com/example/controller/LoginControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+/*
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.assertj.FluentLeniumAssertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.with;
+
+import org.fluentlenium.adapter.junit.FluentTest;
+import org.fluentlenium.core.hook.wait.Wait;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+*/
+public class LoginControllerTest {
+
+}

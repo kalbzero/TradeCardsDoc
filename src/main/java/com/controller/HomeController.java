5aeccae55bc5da1f7869e81125a803f35033506b
Edward Ramos <kalbzero.ramos@gmail.com>
2017-06-02 12:52:32 -0300

    fetch(Home.html): Fragment of home.html and main.html

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -3,12 +3,28 @@ package com.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+/**
+ * 
+ * @author Edward Ramos
+ *
+ */
 @Controller
 public class HomeController {
 	
+	/*
+	 * template/index.html
+	 */
 	@GetMapping("/")
-	public String home(){
+	public String index(){
 		return "index";
 	}
 
+	/*
+	 * template/user/home.html
+	 */
+	@GetMapping("/home")
+	public String home(){
+		return "user/home";
+	}
 }

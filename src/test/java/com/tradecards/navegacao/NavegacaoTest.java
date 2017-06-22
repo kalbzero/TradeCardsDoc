a2a7a28fa5c24c96a69d8859b0882a3f3d9da5bd
Edward Ramos <kalbzero.ramos@gmail.com>
2017-06-22 00:57:55 -0300

    Fetch(NavegacaoTest): Selenium Test in prototypes

--- src/test/java/com/tradecards/navegacao/NavegacaoTest.java
+++ src/test/java/com/tradecards/navegacao/NavegacaoTest.java
@@ -28,20 +28,40 @@ public class NavegacaoTest extends FluentTest{
 		goTo(getUrl("index"));
 		assertThat(window().title()).isEqualTo("TradeCards");
 		
-		$("#email").fill().with("user");
-		$("#senha").fill().with("senha");
-		$("#entrar").submit();
-		assertThat(window().title()).contains("");
+		$("#Entre").click();
+		
+		$("#E-mail").fill().with("user1@email.com");
+		$("#Senha").fill().with("123");
+		$("#Entre").submit();
+		assertThat(window().title()).contains("Home");
 	}
 	
 	@Test
 	public void cadastroTest(){
+		goTo(getUrl("index"));
+		assertThat(window().title()).isEqualTo("TradeCards");
+		
+		$("#Cadastro").click();
+		
+		$("#Nome").fill().with("Tucano");
+		$("#Sobrenome").fill().with("Souza");
+		$("#Telefone").fill().with("051975312468");
+		$("#E-mail").fill().with("user2@email.com");
+		$("#Senha").fill().with("abc");
+		$("#Cadastro").submit();
 		
 	}
 	
 	@Test
 	public void esqueciSenhaTest(){
+		goTo(getUrl("index"));
+		assertThat(window().title()).isEqualTo("TradeCards");
 		
+		$("#Entre").click();
+		assertThat(window().title()).isEqualTo("Recuperar Senha");
+		$("#Recuperar Senha").submit();
+		//Esperar dois segundos
+		assertThat(window().title()).isEqualTo("Enviar");
 	}
 	
 	/* 
@@ -49,12 +69,16 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void homeTest(){
+		goTo(getUrl("userPerfil"));
+		assertThat(window().title()).isEqualTo("Perfil do Usuário");
 		
 	}
 	
 	@Test
 	public void perfilTest(){
-		
+		goTo(getUrl("perfil"));
+		assertThat(window().title()).isEqualTo("Meu Perfil");
+		$("#Alterar Senha").click();
 	}
 	
 	/* 
@@ -62,16 +86,23 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void haveListTest(){
+		goTo(getUrl("haveList"));
+		assertThat(window().title()).isEqualTo("Cartas que Tenho");
 		
 	}
 	
 	@Test
 	public void addCardInHaveListTest(){
+		goTo(getUrl("haveList"));
+		$("#Pesquisar").fill().with("Elfo");
+		$("#Adicionar").click();
 		
 	}
 	
 	@Test
 	public void removeCardInHaveListTest(){
+		goTo(getUrl("haveList"));
+		$("#remove").click();
 		
 	}
 	
@@ -80,16 +111,24 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void wantListTest(){
+		goTo(getUrl("wantList"));
+		assertThat(window().title()).isEqualTo("Cartas que Quero");
 		
 	}
 	
 	@Test
 	public void addCardInWantListTest(){
+		goTo(getUrl("wantList"));
+		assertThat(window().title()).isEqualTo("");
+		$("#Pesquisar").fill().with("Elfo");
+		$("#Adicionar").click();
 		
 	}
 	
 	@Test
 	public void removeCardInWantListTest(){
+		goTo(getUrl("wantList"));
+		assertThat(window().title()).isEqualTo("remove");
 		
 	}
 	
@@ -98,6 +137,9 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void searchCardTest(){
+		goTo(getUrl("searchCard"));
+		assertThat(window().title()).isEqualTo("Buscar Carta");
+		$("#Buscar").click();
 		
 	}
 	
@@ -106,11 +148,19 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void generateReportUsersTest(){
+		goTo(getUrl("generateReport"));
+		assertThat(window().title()).isEqualTo("Gerar Relatórios");
+		$("#Usuários").click();
+		$("#Gerar PDF").click();
 		
 	}
 	
 	@Test
 	public void generateReportCardsTest(){
+		goTo(getUrl("generateReport"));
+		assertThat(window().title()).isEqualTo("Gerar Relatórios");
+		$("#Cartas").click();
+		$("#Gerar PDF").click();
 		
 	}
 	
@@ -119,6 +169,10 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void disableAccountUserTest(){
+		goTo(getUrl("searchUsers"));
+		assertThat(window().title()).isEqualTo("Buscar Usuário");
+		$("#Buscar Usuario").fill().with("Elfo");
+		$("#Buscar").submit();
 		
 	}
 	
@@ -127,22 +181,40 @@ public class NavegacaoTest extends FluentTest{
 	 */
 	@Test
 	public void manageCardsTest(){
+		goTo(getUrl("manageCards"));
+		assertThat(window().title()).isEqualTo("");
 		
 	}
 	
 	@Test
 	public void addCardTest(){
+		goTo(getUrl("manageCards"));
+		assertThat(window().title()).isEqualTo("");
+		$("#Criar").click();
 		
+		$("#Nome").fill().with("Elfo");
+		$("#Edicao").fill().with("2017");
+		$("#Cadastrar").submit();
 	}
 	
 	@Test
 	public void editCardTest(){
+		goTo(getUrl("manageCards"));
+		assertThat(window().title()).isEqualTo("");
+		$("#Buscar").click();
 		
+		$("#Pesquisar").fill().with("Elfo");
+		$("#edit").click();
 	}
 	
 	@Test
 	public void deleteCardTest(){
+		goTo(getUrl("manageCards"));
+		assertThat(window().title()).isEqualTo("");
+		$("#Buscar").click();
 		
+		$("#Pesquisar").fill().with("Elfo");
+		$("#delete").click();
 	}
 	
 }
